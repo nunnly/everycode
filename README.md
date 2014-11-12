4a6c5114b4870932347b908264567a466a194a9c
Nunn <whktk@qq.com>
2014-11-12 12:07:13 +0800

    更新答案

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - 用命令行进入主目录运行`node server.js`
 - 打开`code`文件夹,然后完成代码
 - 打开网页`http://localhost`查看结果
+- 讨论和疑问，可以在在`issue`中进行
+- 答案在wiki中
--- solution/2014-11-12.js
+++
@@ -1,5 +0,0 @@
-function sign(number){
-    var num = new Number(number);
-    console.log(number + ":" + num);
-    return num>0?1:(num==0?0:(num<0?-1:("NaN")));
-}
